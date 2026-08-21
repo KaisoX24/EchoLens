67aded4a96edb325afdb4b143f5bedf829ae521f
Pramit Acharjya <acharjyapramit@gmail.com>
2026-08-21 18:06:55 +0530

    Replace init_chat_model with ChatGroq in llm_client

--- backend/reasoning/llm_client.py
+++ backend/reasoning/llm_client.py
@@ -1,19 +1,27 @@
-from langchain.chat_models import init_chat_model
+from langchain_groq import ChatGroq
 from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
 from functools import lru_cache
+import os
 
 load_dotenv()
 
 @lru_cache
+def get_text_model():
+    return ChatGroq(
+        model='openai/gpt-oss-120b',
+        groq_api_key=os.getenv("GROQ_API_KEY_120")
+    )
+
+@lru_cache
 def get_table_model():
-    return init_chat_model(
-        model='gemini-3.1-flash-lite',
-        model_provider='google_genai',
+    return ChatGroq(
+        model='openai/gpt-oss-20b',
+        groq_api_key=os.getenv("GROQ_API_KEY_20")
     )
 
 @lru_cache
 def get_vision_model():
     return ChatGoogleGenerativeAI(
-        model='gemini-3.5-flash',
+        model='gemini-3.5-flash'
     )
